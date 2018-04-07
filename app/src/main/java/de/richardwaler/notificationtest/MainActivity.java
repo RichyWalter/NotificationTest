550ec766d694ae95aeee16a27d16ec66a2758cf8
RichyWalter <richardwalter@web.de>
2018-04-07 10:20:32 +0200

    Initial commit

---
+++ app/src/main/java/de/richardwaler/notificationtest/MainActivity.java
@@ -0,0 +1,30 @@
+package de.richardwaler.notificationtest;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Notification
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Test Titel")
+                .setContentText("Dies ist ein Test")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        int notificationId = 1;
+
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, mBuilder.build());
+    }
+}
